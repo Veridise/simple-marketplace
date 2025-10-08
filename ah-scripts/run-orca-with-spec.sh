3048bd1431f3c6dfac24c1cca926986cf37974f4
Kostas Ferles <kferles@gmail.com>
2025-10-08 14:32:22 -0500

    Fix minor issue with CI script.

--- ah-scripts/run-orca-with-spec.sh
+++ ah-scripts/run-orca-with-spec.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -e
-
 REVISION=$1
 VERSION_ID=$2
 SPEC=$3
@@ -10,11 +8,10 @@ task_id=$(ah start-orca-task --version-id ${VERSION_ID} --timeout 60 --embedded-
 ah monitor-task --task-id $task_id
 
 task_info_json=$(ah get-task-info --task-id $task_id | grep -v "^+")
-orca_findings_id=$(echo "$task_info_json" | jq -r '.artifacts[] | select(.name == "orca/findings.json") | .id')
+orca_findings_id=$(echo "$task_info_json" | jq -r '.artifacts[] | select(.name == "findings.json") | .id')
 ah get-task-artifact --task-id $task_id --artifact-id $orca_findings_id --output-file ./orca_findings_id.json
 ORCA_CEXES=$(jq '.findings.critical' ./orca_findings_id.json)
 
 if ((${ORCA_CEXES} != 0)); then
-    echo "OrCa found a counterexample for spec: ${SPEC}"
     exit 1
 fi
