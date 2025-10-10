8e6013dba8ff3815c244420448f37114f1bf8251
Nikos Chondros <nikos@veridise.com>
2025-10-10 15:35:11 +0300

    Simplify based on new AuditHub client capabilities
    Use `ah get-task-info --verify` to simplify the scripts
    Use the new `ah download-artifact` to simplify waiting for artifacts

--- ah-scripts/run-orca-with-spec.sh
+++ ah-scripts/run-orca-with-spec.sh
@@ -6,12 +6,4 @@ SPEC=$3
 
 task_id=$(ah start-orca-task --version-id ${VERSION_ID} --timeout 60 --embedded-specs ${SPEC})
 ah monitor-task --task-id $task_id
-
-task_info_json=$(ah get-task-info --task-id $task_id | grep -v "^+")
-orca_findings_id=$(echo "$task_info_json" | jq -r '.artifacts[] | select(.name == "findings.json") | .id')
-ah get-task-artifact --task-id $task_id --artifact-id $orca_findings_id --output-file ./orca_findings_id.json
-ORCA_CEXES=$(jq '.findings.critical' ./orca_findings_id.json)
-
-if ((${ORCA_CEXES} != 0)); then
-    exit 1
-fi
+ah get-task-info --verify --output none
--- ah-scripts/run-vanguard.sh
+++ ah-scripts/run-vanguard.sh
@@ -8,12 +8,16 @@ TIMESTAMP=$(date -u +"%Y-%m-%dT%H:%M:%S.%3NZ")
 
 version_id=$(ah create-version-via-url --name "@ga-${TIMESTAMP}-${REVISION:0:7}" --input-type git --url https://github.com/Veridise/simple-marketplace.git --revision $REVISION)
 task_id=$(ah start-defi-vanguard-task --version-id ${version_id} --detector unchecked-return use-before-def)
-ah monitor-task --task-id $task_id
+ah monitor-task --task-id $task_id 
 
 task_info_json=$(ah get-task-info --task-id $task_id | grep -v "^+")
-for sarif_id in `echo "$task_info_json" | jq -r '.artifacts[] | select(.name == "sarif.json") | .id'`
-do
-    ah get-task-artifact --task-id $task_id --artifact-id $sarif_id --output-file "./"$sarif_id".sarif" --log-level ERROR
-    detector_name=$(jq -r '.runs[0].tool.driver.rules[0].id' "./"$sarif_id".sarif")
-    mv "./"$sarif_id".sarif" "./"$detector_name".sarif"
+
+mapfile -t detector_steps < <(echo "$task_info_json" | jq -r '.steps[] | select(.definition.is_tool) | .code')
+
+for detector_step in "${detector_steps[@]}"; do
+    # remove any funny characters from the step code to make sure we can preserve a file in the current directory
+    sarif_file_name="./${detector_step//[^A-Za-z0-9._-]/_}.sarif"
+    ah download-artifact --task-id $task_id --step-code "$detector_step" --name sarif.json --output-file "$sarif_file_name" --log-level ERROR
+    detector_name=$(jq -r '.runs[0].tool.driver.rules[0].id' "$sarif_file_name")
+    mv "$sarif_file_name" "./$detector_name.sarif"
 done
